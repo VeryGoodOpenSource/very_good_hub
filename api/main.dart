e3673f94c052bb47a5706118b4a3a2b5a43844fb
Erick Zanardo <erickzanardoo@gmail.com>
2023-07-25 09:55:51 -0300

    PR suggetions

--- api/main.dart
+++ api/main.dart
@@ -3,8 +3,13 @@ import 'dart:io';
 import 'package:dart_frog/dart_frog.dart';
 import 'package:db_client/db_client.dart';
 
+late final DbClient dbClient;
+
+Future<void> init(InternetAddress ip, int port) async {
+  dbClient = DbClient();
+}
+
 Future<HttpServer> run(Handler handler, InternetAddress ip, int port) async {
-  final dbClient = DbClient();
   return serve(
     handler.use(provider<DbClient>((_) => dbClient)),
     ip,
