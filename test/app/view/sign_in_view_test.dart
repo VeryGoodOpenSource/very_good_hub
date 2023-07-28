da173d24ee4a2fde2a60ecf86ace6560e603402a
Erick Zanardo <erickzanardoo@gmail.com>
2023-07-28 16:10:03 -0300

    lint

--- test/app/view/sign_in_view_test.dart
+++ test/app/view/sign_in_view_test.dart
@@ -4,7 +4,6 @@ import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:hub_domain/hub_domain.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:very_good_hub/app/app.dart';
 
