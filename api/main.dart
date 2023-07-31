704f67e2f7061bd86dd6503790600b77c6d78626
Erick Zanardo <erickzanardoo@gmail.com>
2023-07-31 13:26:28 -0300

    feat: making db client in api main private

--- api/main.dart
+++ api/main.dart
@@ -3,15 +3,15 @@ import 'dart:io';
 import 'package:dart_frog/dart_frog.dart';
 import 'package:db_client/db_client.dart';
 
-late final DbClient dbClient;
+late final DbClient _dbClient;
 
 Future<void> init(InternetAddress ip, int port) async {
-  dbClient = DbClient();
+  _dbClient = DbClient();
 }
 
 Future<HttpServer> run(Handler handler, InternetAddress ip, int port) async {
   return serve(
-    handler.use(provider<DbClient>((_) => dbClient)),
+    handler.use(provider<DbClient>((_) => _dbClient)),
     ip,
     port,
   );
